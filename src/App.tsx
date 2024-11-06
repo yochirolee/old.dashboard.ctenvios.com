8aa80b75ba3a9ac325c06c7f182dda1e8bf9eddd
yosho <yleecruz@gmail.com>
2024-11-06 09:46:56 -0500

    toaster import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Layout from "@/layout/layout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@/context/theme-context";
-import { AppRouter } from "./router/AppRouter";
+import { AppRouter } from "@/router/AppRouter";
 
 const queryClient = new QueryClient();
 
